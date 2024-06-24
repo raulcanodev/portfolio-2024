84719f0722695a96af3d8cdd8076c1e1a06787c1
Raul M <118123543+pillow3@users.noreply.github.com>
2024-06-24 07:13:34 +0100

    test

--- src/utils.ts
+++ src/utils.ts
@@ -18,33 +18,3 @@ function capitalize(str: string): string {
 }
 
 export { formatDate, capitalize };
-
-/**
- * Returns collection entries matching a value for a given field
- * @param {string} type - The type of collection such as "blog"
- * @param {string} term - The field to search on such as "tags" or "author"
- * @param {string} value - The value to match such as "Popular"
- * @param {boolean} reverse - Sort with oldest posts first
- * @returns {Array<Object>}
- */
-export const getCollectionFilter = async (
-	type: string,
-	term: string,
-	value: string,
-	reverse = false
-): Promise<Array<Object>> => {
-	const items = await getCollection(type, true, reverse);
-
-	const slug = slugify(value);
-
-	return items.filter((item: any) => {
-		const values: Array<string> = [];
-		if (Array.isArray(item.data[term])) {
-			values.push(...item.data[term]);
-		} else if (item.data[term]?.slug) {
-			values.push(item.data[term].slug);
-		}
-
-		return values.map((value) => slugify(value)).includes(slug);
-	});
-};
