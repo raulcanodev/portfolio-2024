25da638ca50ed2260c4e656dd01488597c488fb6
Raul M <118123543+pillow3@users.noreply.github.com>
2024-07-06 14:54:27 +0100

    devops: vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), react()],
-  output: 'server'
+  output: 'server',
+  adapter: vercel()
 });
-
