cbd55656633e56f98b38b04420d6513856b61fab
Raul Marco Cano <raul@widgilabs.com>
2024-10-15 17:44:32 +0100

    now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
-import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react()],
+  integrations: [tailwind(), mdx()],
   output: 'server',
   adapter: vercel(),
 });
