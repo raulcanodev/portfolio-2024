6aed001cf8a0d10646f9867479474da4c77d5097
Raul Marco Cano <raul@widgilabs.com>
2024-10-15 17:28:23 +0100

    test

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/add-your-folder-path-to-your-file-in-visual-studio-code_Cs_-mRof.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./add-your-folder-path-to-your-file-in-visual-studio-code_D97iKv6N.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/from-factory-worker-to-developer_C2SCXOwJ.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./from-factory-worker-to-developer_rY0VHouu.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
